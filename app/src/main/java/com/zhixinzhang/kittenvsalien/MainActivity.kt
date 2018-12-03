487fac916a339c6c1a8f676164b9215e0f66314f
Zhixin Zhang <r914721222@gmail.com>
2018-12-03 17:39:50 -0500

    Tested picasso

--- app/src/main/java/com/zhixinzhang/kittenvsalien/MainActivity.kt
+++ app/src/main/java/com/zhixinzhang/kittenvsalien/MainActivity.kt
@@ -2,11 +2,17 @@ package com.zhixinzhang.kittenvsalien
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.squareup.picasso.Picasso
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var pic = Picasso.get().load("http://i.imgur.com/DvpvklR.png")
+        pic.into(previewImgView)
+        pic.into(previewImgView2)
     }
 }
